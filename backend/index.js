0bfaec92e820530d3f41b70a3ab0706ee0f6f9fe
AJINKYA <ajinkyacodes@gmail.com>
2023-12-19 03:47:23 +0530

    Created Display page view simple option and commented

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,10 @@ app.use(
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type'],
     })
-)
+);
 
 // Display Page View for root directory of the backend
+// const displayPageView = "connected to server"; //Optional
 const displayPageView = `
 <div style="height:100%; display:-webkit-box; display:-ms-flexbox; display:flex; -webkit-box-orient: vertical; -webkit-box-direction: normal; -ms-flex-direction: column; flex-direction: column; -webkit-box-align:center; -ms-flex-align:center; align-items:center; -webkit-box-pack:center; -ms-flex-pack:center; justify-content:center;">
     <h1 style="font-family:sans-serif; font-size: 48px; text-align:center;">Book Store App</h1>
