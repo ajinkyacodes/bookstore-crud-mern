309b2d318216e674f8209acd510cbfeb94b5f4fa
AJINKYA <ajinkyacodes@gmail.com>
2023-12-19 02:23:38 +0530

    created route for updating a single book data by id

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
 import { Book } from "./models/bookModel.js";
@@ -60,6 +60,31 @@ app.get('/books/:id', async(request, response) => {
         const book = await Book.findById(id);
         
         return response.status(200).json(book);
+    } catch(error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Route for Update a Book
+app.put('/books/:id', async(request, response) => {
+    try {
+        if(!request.body.title || !request.body.author || !request.body.publishYear) {
+            return response.status(400).send({
+                message: "Send all required fields: title, author, publishYear",
+            });
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if(!result) {
+            response.status(500).send({ message:"Book not found" });
+        }
+
+        return response.status(200).send({message: "Book updated successfully"});
+
     } catch(error) {
         console.log(error.message);
         response.status(500).send({message: error.message});
